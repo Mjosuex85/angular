7a2c5fd820c4a7e48acc5607c6c477466a42bc87
Mario Vidal <mario.josuevp@gmail.com>
2023-10-15 05:45:01 +0200

    Modulot 63 forms

--- 02-bases/src/app/dbz/dbz.module.ts
+++ 02-bases/src/app/dbz/dbz.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component'
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 
 @NgModule({
   declarations: [
+    AddCharacterComponent,
+    ListComponent,
     MainPageComponent
   ],
 
@@ -15,7 +20,8 @@ import { MainPageComponent } from './pages/main-page.component';
   ],
 
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class DbzModule { }
